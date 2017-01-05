e76957cd59ab147c742235db4b8c07a731097f8b
yuki.takahashi <yuki.takahashi.1126@gmail.com>
2017-01-05 22:18:19 +0900

    Clean-up project

---
+++ include/pigeon/Parse/lexer.hpp
@@ -0,0 +1,14 @@
+#ifndef PIGEON_PARSE_LEXER_H
+#define PIGEON_PARSE_LEXER_H
+
+#include <iostream>
+#include <stdlib.h>
+
+namespace pigeon {
+    class Lexer {
+        public:
+            void test();
+    };
+}
+
+#endif
---
+++ lib/Parse/lexer.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <stdlib.h>
+#include "pigeon/Parse/lexer.hpp"
+
+void pigeon::Lexer::test() {
+    std::cout << "test!! OK!!" << std::endl;
+}
