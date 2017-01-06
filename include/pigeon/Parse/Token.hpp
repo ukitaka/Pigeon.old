fe81a80663c030da1dc1fd4b3bac1eb4161d955f
yuki.takahashi <yuki.takahashi.1126@gmail.com>
2017-01-06 23:20:05 +0900

    Add lexer, tokens

---
+++ include/pigeon/Parse/Token.hpp
@@ -0,0 +1,13 @@
+//
+//  Token.h
+//  pigeon
+//
+//  Created by Yuki Takahashi on 2017/01/06.
+//
+//
+
+#ifndef PIGEON_PARSE_TOKEN_H
+#define PIGEON_PARSE_TOKEN_H
+
+
+#endif /* PIGEON_PARSE_TOKEN_H */
