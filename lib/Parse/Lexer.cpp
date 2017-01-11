f526e1c54c839f07e46a138f779acbbc6853da23
yuki.takahashi <yuki.takahashi.1126@gmail.com>
2017-01-12 01:36:13 +0900

    Add `*` operator

--- lib/Parse/Lexer.cpp
+++ lib/Parse/Lexer.cpp
@@ -59,7 +59,7 @@ Restart:
       goto Restart;
     }
     return formToken(tok::eof, TokStart);
-  case '+':
+  case '+': case '*':
     return formToken(tok::oper_binary, TokStart);
   case '0': case '1': case '2': case '3': case '4':
   case '5': case '6': case '7': case '8': case '9':
