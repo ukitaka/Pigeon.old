38f70fc0051ee96067fbdad2c04b072685b931c2
yuki.takahashi <yuki.takahashi.1126@gmail.com>
2017-01-18 21:23:58 +0900

    Add SourceFile.hpp

---
+++ include/pigeon/AST/SourceFile.hpp
@@ -0,0 +1,21 @@
+//
+//  SourceFile.hpp
+//  pigeon
+//
+//  Created by Yuki Takahashi on 2017/01/17.
+//
+//
+
+#ifndef PIGEON_AST_SOURCE_H
+#define PIGEON_AST_SOURCE_H
+
+#include <memory>
+
+namespace pigeon {
+  class SourceFile {
+  public:
+      SourceFile(unsigned BufferID);
+  };
+}
+
+#endif /* PIGEON_AST_SOURCE_H */
