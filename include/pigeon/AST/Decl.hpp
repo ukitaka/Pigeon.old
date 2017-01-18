118e3dfe0a1b6c21a80a0f7132f04df5687309df
yuki.takahashi <yuki.takahashi.1126@gmail.com>
2017-01-18 21:30:34 +0900

    Add Decl

---
+++ include/pigeon/AST/Decl.hpp
@@ -0,0 +1,18 @@
+//
+//  Decl.hpp
+//  pigeon
+//
+//  Created by Yuki Takahashi on 2017/01/18.
+//
+//
+
+#ifndef PIGEON_AST_DECL_H
+#define PIGEON_AST_DECL_H
+
+namespace pigeon {
+    class Decl {
+        
+    };
+}
+
+#endif /* Decl_hpp */
---
+++ lib/AST/Decl.cpp
@@ -0,0 +1,9 @@
+//
+//  Decl.cpp
+//  pigeon
+//
+//  Created by Yuki Takahashi on 2017/01/18.
+//
+//
+
+#include "pigeon/AST/Decl.hpp"
