374b46dec1cf54583dc13ced8a5e1489992b5fa8
yuki.takahashi <yuki.takahashi.1126@gmail.com>
2017-01-19 21:02:58 +0900

    Move `TypeKind` to hpp

--- include/pigeon/AST/Type.hpp
+++ include/pigeon/AST/Type.hpp
@@ -9,4 +9,15 @@
 #ifndef PIGEON_AST_SOURCE_H
 #define PIGEON_AST_SOURCE_H
 
+namespace pigeon {
+enum class TypeKind { BuiltinVoid, BuiltinInt };
+
+class Type {
+  TypeKind Kind;
+
+public:
+  TypeKind getKind() const { return Kind; }
+};
+}
+
 #endif /* PIGEON_AST_SOURCE_H */
--- lib/AST/Type.cpp
+++ lib/AST/Type.cpp
@@ -7,14 +7,3 @@
 //
 
 #include "pigeon/AST/Type.hpp"
-
-namespace pigeon {
-enum class TypeKind { BuiltinVoid, BuiltinInt };
-
-class Type {
-  TypeKind Kind;
-
-public:
-  TypeKind getKind() const { return Kind; }
-};
-}
