e8537648998e2b0a3c3a6a161113098fe38a905b
yuki.takahashi <yuki.takahashi.1126@gmail.com>
2017-01-19 23:46:48 +0900

    Add Operator

---
+++ include/pigeon/AST/Operator.hpp
@@ -0,0 +1,29 @@
+//
+//  Operator.hpp
+//  pigeon
+//
+//  Created by Yuki Takahashi on 2017/01/19.
+//
+//
+
+#ifndef PIGEON_AST_OPERATOR_H
+#define PIGEON_AST_OPERATOR_H
+
+namespace pigeon {
+    enum class OperatorKind {
+        Add,
+        Sub,
+        Mul,
+        Shl
+    };
+    
+    class Operator { };
+    class BinaryOperator: public Operator {
+        const OperatorKind Kind;
+    public:
+        BinaryOperator(OperatorKind Kind);
+        OperatorKind getKind() const { return Kind; };
+    };
+}
+
+#endif /* PIGEON_AST_OPERATOR_H */
---
+++ lib/AST/Operator.cpp
@@ -0,0 +1,15 @@
+//
+//  Operator.cpp
+//  pigeon
+//
+//  Created by Yuki Takahashi on 2017/01/19.
+//
+//
+
+#include "pigeon/AST/Operator.hpp"
+
+using namespace pigeon;
+
+BinaryOperator::BinaryOperator(OperatorKind Kind): Kind(Kind) {
+
+}
