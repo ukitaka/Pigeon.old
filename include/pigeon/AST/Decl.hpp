d165d5bd16494ffa45ce1681fc8e496f6786ca47
yuki.takahashi <yuki.takahashi.1126@gmail.com>
2017-01-20 00:06:08 +0900

    Add Expr to VarDecl

--- include/pigeon/AST/Decl.hpp
+++ include/pigeon/AST/Decl.hpp
@@ -9,6 +9,7 @@
 #ifndef PIGEON_AST_DECL_H
 #define PIGEON_AST_DECL_H
 
+#include "pigeon/AST/Expr.hpp"
 #include "pigeon/AST/Identifier.hpp"
 #include "pigeon/AST/Type.hpp"
 #include <memory>
@@ -20,6 +21,7 @@ class Decl {};
 class VarDecl : public Decl {
   std::shared_ptr<Type> Type;
   std::shared_ptr<Identifier> Identifier;
+  std::shared_ptr<Expr> Expr;
 };
 }
 
