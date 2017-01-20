45278b5cf5270183f61c1e159129c692c20c84ea
yuki.takahashi <yuki.takahashi.1126@gmail.com>
2017-01-21 00:24:06 +0900

    Add constructor

--- include/pigeon/AST/Expr.hpp
+++ include/pigeon/AST/Expr.hpp
@@ -18,25 +18,39 @@ namespace pigeon {
 enum class ExprKind {
   IntegerLiteral,
   Binary,
+  Paren,
 };
 
 class Expr {
   ExprKind Kind;
+
+public:
+  ExprKind getKind() const { return Kind; }
   Expr(ExprKind Kind);
 };
 
 class IntegerLiteral : public Expr {
   llvm::StringRef Val;
+
+public:
+  IntegerLiteral(llvm::StringRef Val);
 };
 
 class ParenExpr : public Expr {
   std::shared_ptr<Expr> SubExpr;
+
+public:
+  ParenExpr(std::shared_ptr<Expr> SubExpr);
 };
 
 class BinaryExpr : public Expr {
   std::shared_ptr<Expr> LHS;
   std::shared_ptr<Operator> Oper;
   std::shared_ptr<Expr> RHS;
+
+public:
+  BinaryExpr(std::shared_ptr<Expr> LHS, std::shared_ptr<Operator> Oper,
+             std::shared_ptr<Expr> RHS);
 };
 }
 
--- lib/AST/Expr.cpp
+++ lib/AST/Expr.cpp
@@ -7,7 +7,19 @@
 //
 
 #include "pigeon/AST/Expr.hpp"
+#include <memory>
 
 using namespace pigeon;
 
 Expr::Expr(ExprKind Kind) : Kind(Kind) {}
+
+IntegerLiteral::IntegerLiteral(llvm::StringRef Val)
+    : Expr(ExprKind::IntegerLiteral), Val(Val) {}
+
+ParenExpr::ParenExpr(std::shared_ptr<Expr> SubExpr)
+    : Expr(ExprKind::Paren), SubExpr(SubExpr) {}
+
+BinaryExpr::BinaryExpr(std::shared_ptr<Expr> LHS,
+                       std::shared_ptr<Operator> Oper,
+                       std::shared_ptr<Expr> RHS)
+    : Expr(ExprKind::Binary), LHS(LHS), Oper(Oper), RHS(RHS) {}
